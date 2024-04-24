5c8c11641319f6fa0eac082a94410efbd9a0490c
TransistorTun <phamhoanhtuan140403@gmail.com>
2024-04-24 13:04:35 +0700

    update firmware

---
+++ firm_ware_esp32/test/build/esp-idf/mbedtls/mbedtls/library/error.c
@@ -0,0 +1,1 @@
+/home/pham_tuan/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
---
+++ firm_ware_esp32/test/build/esp-idf/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
@@ -0,0 +1,1 @@
+/home/pham_tuan/esp/esp-idf/components/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
